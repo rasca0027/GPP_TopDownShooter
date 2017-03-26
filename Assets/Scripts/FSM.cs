a46364b29fb3bed19f0820aade8c18a53d4754d9
rasca0027 <rasca0027@gmail.com>
2017-03-26 11:06:46 -0400

    basic FSM

---
+++ Assets/Scripts/FSM.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FSM : MonoBehaviour {
+
+    // We keep track of the state machine's current state and expose it through a public
+    // property in case someone needs to query it.
+    public State CurrentState { get; private set; }
+
+    // We don't want to change the current state in the middle of an update, so when a transition is called
+    private State _pendingState;
+
+    void Start() 
+    {
+        TransitionTo(new NormalState());
+    }
+
+    public void Update()
+    {
+        // Handle any pending transition if someone called TransitionTo externally (although they probably shouldn't) 
+        PerformPendingTransition();
+        // Make sure there's always a current state to update...
+        Debug.Assert(CurrentState != null, "No Current State!");
+        CurrentState.Update();
+        // Handle any pending transition that might have happened during the update
+        PerformPendingTransition();
+    }
+
+    // Queues transition to a new state
+    public void TransitionTo(State TState)
+    {
+        // We do the actual transtion 
+        _pendingState = TState;
+    }
+
+    // Actually transition to any pending state
+    private void PerformPendingTransition()
+    {
+        if (_pendingState != null)
+        {
+            if (CurrentState != null) CurrentState.OnExit();
+            CurrentState = _pendingState;
+            CurrentState.OnEnter();
+            _pendingState = null;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class State {
+
+    // A convenience method for transitioning from inside a state
+    protected void TransitionTo(State TState)
+    {
+	Service.fsm.TransitionTo(TState);
+    }
+    // I don't need Init because I did not use cache
+    // This is called whenever the state becomes active (think of it like Unity's Start)
+    public virtual void OnEnter() { }
+
+    // this is called whenever the state becomes inactive
+    public virtual void OnExit() { }
+
+    // This is your standard update method where most of your work should go
+    public virtual void Update() { }
+
+    // called when the state machine is cleared, and where you should clear resources
+    public virtual void CleanUp() { }
+
+}
+
+public class NormalState : State {
+
+    public override void OnEnter() {
+        Debug.Log("in normal state!");
+    }
+
+    public override void Update() {
+        // if health too low go to lowHealthState
+        int health = GameObject.Find("Player").GetComponent<PlayerHealth>().health;
+        if (health <= 3)
+            TransitionTo(new LowHealthState());
+        // if hit 2 enemy in a row go to bonus state
+        
+    }
+
+
+}
+
+public class BonusState : State {
+
+    public override void OnEnter() {
+        Debug.Log("in bonus state!");
+    }
+
+    public override void Update() {
+        // change background or add effects
+        // twice score
+        // after 5 seconds go back to normal state
+    }
+
+}
+
+public class LowHealthState : State {
+
+    public override void OnEnter() {
+        Debug.Log("in low health state!");
+    }
+
+    public override void Update() {
+        // put a heart beating?
+
+        // if die go back to normal state
+    }
+
+}
